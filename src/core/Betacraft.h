c1ca935ef93ddf0210711744f408ed7390262b32
Moresteck <moresteck@protonmail.com>
2023-06-23 13:32:43 +0200

    Version update

--- src/core/Betacraft.h
+++ src/core/Betacraft.h
@@ -5,7 +5,7 @@
 
 #include <limits.h>
 
-#define BETACRAFT_VERSION "2.0.0-alpha.20230604"
+#define BETACRAFT_VERSION "2.0.0-alpha.20230623"
 
 extern int betacraft_online;
 extern char application_support_path[PATH_MAX];
